f7e0a68a0a5a35acbec781c7fc3bb9170a1c5749
Rohith Vallu <vrohitrao@gmail.com>
2015-10-18 01:33:56 -0400

    LeetCode and Arrays Build on 10/07/2015 Author : Rohith Vallu

---
+++ LeetCode/src/NimGame.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Rohith Vallu on 10/18/2015.
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+            return false;
+        else
+            return true;
+    }
+}
